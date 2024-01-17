1397859fb6dad633505ffdde8557179440223146
mohd ruman <mohd@ruman.org>
2024-01-17 23:00:53 +0530

    3d Model Completed + loading progress + fixed breaking while loading

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,11 +11,23 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
+      <hemisphereLight intensity={1}
       groundColor="black"/>
-      <pointLight intensity={1}/>
+      <pointLight intensity={9}/>
+      <spotLight
+        intensity={1}
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -4, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
