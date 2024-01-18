6c62558f4b221e1fd457073a27ba0908b5c2869f
mohd ruman <mohd@ruman.org>
2024-01-18 10:47:20 +0530

    added scrolldown icon and animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,6 +32,32 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas/>
+
+      <div className='absolute xs:bottom-10
+      bottom-32 w-full flex justify-center
+      items-center'>
+        <a href="#about">
+          <div className="w-[32px] h-[62px]
+          border-4 rounded-3xl border-secondary
+          flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y:[0, 24, 0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat: Infinity,
+                repeatType:'loop'
+              }}
+
+              className="w-3 h-3 bg-secondary 
+              rounded-full mb-1"
+            />
+
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
