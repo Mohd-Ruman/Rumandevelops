6e8a53c163eb9b12a18b81271b6515bb31ee861a
mohd ruman <mohd@ruman.org>
2024-01-18 11:39:54 +0530

    Hero Section completed : Exeption event listner for mobile scroll

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../loader';
 
-const Computers = () => {
+const Computers = ( {isMobile} ) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
   
@@ -25,7 +25,7 @@ const Computers = () => {
 
       <primitive
         object={computer.scene}
-        scale={0.75}
+        scale={isMobile ? 0.7 : 0.75}
         position={[0, -4, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -35,6 +35,33 @@ const Computers = () => {
 
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(()=>{
+    //Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    //set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    //Define a callback funtion to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+
+    //Add an event listerner 
+    mediaQuery.addEventListener("change",
+    handleMediaQueryChange);
+ 
+    // Remove the listener when the component is unmounted
+    return() => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+
+  }
+
+});
+
   return (
     <Canvas
       frameloop='demand'
@@ -48,7 +75,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI/2}
           minPolarAngle={Math.PI/2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
     
       <Preload all/>
