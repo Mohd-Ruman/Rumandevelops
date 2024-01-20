f94468592749a59613d1e4f08ed7c24d7e517070
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-20 06:52:07 +0530

    Added cards in about section with titles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import {motion} from 'framer-motion'
+
+import {styles} from '../styles'
+import {services} from '../constants'
+import {fadeIn, textVariant} from '../utils/motion'
+
+const ServiceCard = ({index, title, icon}) => {
+  return(
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-cardSS'>
+
+        <div options={{
+          max : 45,
+          scale : 1,
+          speed : 450
+        }}
+        className='min-h-[250px] bg-tertiary rounded-[20px]
+        py-5 px-12 flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt="icon"
+          className="w-16 h-16 object-contain"/>
+          <h3 className='font-bold text-white text-[20px] text-center'>
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("","", 0.1, 1)}
+      className='mt-4 max-w-3xl text-secondary text-[17px] leading-[30px]'>
+        I'm a skilled frontend developer that uses his graphic
+        designing skills as a boost to create seamless and eye
+        melting visuals. I work on technologies like React, Git and Github,
+        Javascript, ASP.NET, C#, Entity core framework, SQL, NodeJS.
+        </motion.p>
+
+      <div className='flex gap-10 mt-20 flex-wrap'>
+        {services.map((service, index)=>(
+          <ServiceCard key={service.title} index={index}
+          {...service}/>
+
+        ))}
+      </div>
+    </>
   )
 }
 
