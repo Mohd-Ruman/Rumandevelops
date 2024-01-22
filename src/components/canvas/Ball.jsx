4124b519c3d5ed385d590c5291afe5d6b8ab99aa
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-23 03:18:09 +0530

    Work section completed : Icon Balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -16,6 +16,17 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.5]}/>
       <mesh castShadow recievedShadow scale={2.75}>
       <icosahedronGeometry args={[1, 1]} />
+      <meshStandardMaterial
+       color='#fff8eb'
+       polygonOffset
+       polygonOffsetFactor={-5}
+       flatShading
+      />
+      <Decal
+        map={decal}
+        position={[0,0,1]}
+        rotation={[2 * Math.PI, 0, 6.25]}
+      />
       </mesh>
     </Float>
   )
