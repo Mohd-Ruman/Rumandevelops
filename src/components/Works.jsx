43468600e4087dad131760bb656e74df99b2798d
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-23 04:23:03 +0530

    Work section : Project cards tilting functionality

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,11 +8,57 @@ import { github } from '../assets'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ProjectCard = ({index, name, description, tags,
+image, source_code_Link}) => {
+  return(
+    <motion.div 
+    variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt 
+      options={{
+        max : 45,
+        scale: 1,
+        speed : 450
+      }}
+      className='bg-tertiary p-5 rounded-2xl
+      sm:w-[360px] w-full'
+      //continue from here 23 Jan MIDNIGHT
+      >
+
+      </Tilt>
+    </motion.div>
+  )
+} 
 
 const Works = () => {
   return (
     <>
-      works
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p 
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px]
+          max-w-3xl leading-[30px]'
+        >
+          Following projects showcase my skills and
+          experience through real-world examples of
+          my work. Each project is briefly described
+          with links to code repositories and live 
+          demos in it. It reflects my ability to solve
+          complex problems, work with different technologies
+          and manage projects effectively
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index)=>(
+          <ProjectCard key={`project-${index}`}
+          index={index}/>
+        ))}
+      </div>
     </>
   )
 }
