e7592f3817826f4191bde864e4f711638ffacf3a
Mohd-Ruman <mohd.ruman.82@gmail.com>
2024-01-23 03:53:26 +0530

    Project/work section imports done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {motion} from 'framer-motion'
+import Tilt from 'react-parallax-tilt'
+
+import { styles } from '../styles'
+import { SectionWrapper } from '../hoc'
+import { github } from '../assets'
+import { projects } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      works
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, '');
