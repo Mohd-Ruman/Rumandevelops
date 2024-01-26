4eb98ba8670e0db4d9737e7ba569d5de41d28e1d
mohd ruman <mohd.ruman.82@gmail.com>
2024-01-26 23:38:23 +0530

    Contact Form working : Test complete, Website Ready to deploy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,9 +22,43 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const {name, value} = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({...form, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_lqhtqhd',
+      'template_qksgmuo',
+      {
+        from_name: form.name,
+        to_name: 'Ruman',
+        from_email: form.email,
+        to_email: 'mohd.ruman.82@gmail.com',
+        message: form.message,
+      },
+      'GB7a6bmKUG-gqfuGE'
+    )
+    .then(() => {
+      setLoading(false);
+      alert("Thank you. I will get back to you as soon as possible.");
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }), (error) => {
+      setLoading(false)
+      console.log(error);
+      alert("Sorry, Something went wrong.");
+    }
+  }
 
   return (
    <div className="xl:mt-12 xl:flex-row flex-col-reverse
